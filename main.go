f5394e47be60580466cf20751f25952653033d14
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-05-25 18:59:33 +0900

    Add CLI function

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"os/signal"
 	"sort"
 
+	hs "./HTTPServer"
+
+	"./channel"
+	"./datastore"
+	"./proxy"
 	cli "github.com/urfave/cli"
 )
 
@@ -28,20 +33,40 @@ func main() {
 	}
 
 	sort.Sort(cli.FlagsByName(app.Flags))
+
+	m := channel.NewMatchChannel()
+	pr := proxy.NewProxy(m)
+	hs := hs.NewHTTPServer(m)
+	hh := NewHHTTPmanager(m)
+
+	app.Action = func(c *cli.Context) {
+		db := datastore.Database{Database: "./test.db"}
+		dbschema := []interface{}{proxy.History{}, proxy.Request{}, proxy.Response{}}
+		go func() {
+			for _, v := range dbschema {
+				db.Table = v
+				db.InitMigration()
+			}
+		}()
+		go pr.Run()
+		go hs.Run()
+		go hh.Run()
+		sigClose(m)
+	}
 	// アプリケーションの起動
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	sigClose()
 }
 
 // ctrl + c用の
-func sigClose() {
+func sigClose(m *channel.Matchlock) {
 	// シグナル用のチャネル定義
 	quit := make(chan os.Signal)
 	// 受け取るシグナルを設定
 	signal.Notify(quit, os.Interrupt)
+	//<-m.ExitSignal
 	<-quit // ここでシグナルを受け取るまで以降の処理はされない
 }
