756c8d71b4fe30041d019a15270e31e853883507
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-05-25 23:29:51 +0900

    Add action for history As io.ReadCloser is closed, it corresponds temporarily.

--- httpmanagement.go
+++ httpmanagement.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+	"reflect"
+	"strings"
+
 	"./channel"
 )
 
@@ -11,14 +16,42 @@ type HTTPmanager struct {
 func (h *HTTPmanager) Run() {
 	reqchan := h.channels.Request
 	reschan := h.channels.Response
+	histry := History{}
 	for {
 		select {
 		case req := <-reqchan.ProxToHMgSignal:
-			reqchan.HMgToHsSignal <- req
-			req = <-reqchan.HMgToHsSignal
-			reqchan.ProxToHMgSignal <- req
+			bufbody := new(bytes.Buffer)
+			bufbody.ReadFrom(req.Body)
+			bstr := bufbody.String()
+			req.Body = ioutil.NopCloser(strings.NewReader(bstr))
+
+			if h.channels.IsForward {
+				reqchan.HMgToHsSignal <- req
+				histry.SetIdentifier(GetSha1(req.URL.String()))
+				go histry.MemoryRequest(req, false, bstr)
+				creq := <-reqchan.HMgToHsSignal
+
+				bufbody = new(bytes.Buffer)
+				bufbody.ReadFrom(creq.Body)
+				bstr, creq.Body = bufbody.String(), ioutil.NopCloser(strings.NewReader(bstr))
+
+				reqchan.ProxToHMgSignal <- creq
+				if reflect.DeepEqual(req, creq) != true {
+					go histry.MemoryRequest(creq, true, bstr)
+				}
+			} else {
+				histry.SetIdentifier(GetSha1(req.URL.String()))
+				go histry.MemoryRequest(req, false, bstr)
+				reqchan.ProxToHMgSignal <- req
+			}
 		case res := <-reschan.ProxToHMgSignal:
+			bufbody := new(bytes.Buffer)
+			bufbody.ReadFrom(res.Body)
+			bstr := bufbody.String()
+			res.Body = ioutil.NopCloser(strings.NewReader(bstr))
 			reschan.ProxToHMgSignal <- res
+			histry.MemoryResponse(res, bstr)
+			histry = History{}
 		}
 	}
 }
