415006bced73f7e21a5caec5534a171f7436da40
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-05-30 12:31:14 +0900

    Changed http history to DB write only

--- history/history.go
+++ history/history.go
@@ -19,12 +19,16 @@ func (h *History) SetIdentifier(id string) {
 func (h *History) MemoryRequest(r *http.Request, isEdit bool, bstr string) {
 	fmt.Println(bstr)
 	var req = Request{Identifier: h.Identifier, IsEdit: isEdit}
-	req.SetRequest(r, bstr)
+	go req.SetRequest(r, bstr)
+	go req.SetHeader(r.Header)
+	go req.SetData(bstr, r.ContentLength, r.TransferEncoding)
 	db.Table = History{}
 	db.Insert(h)
 }
 
 func (h *History) MemoryResponse(r *http.Response, bstr string) {
 	var res = Response{Identifier: h.Identifier}
-	res.SetResponse(r)
+	go res.SetResponse(r)
+	go res.SetResponseHeader(r.Header)
+	go res.SetResponseBody(bstr, r.ContentLength, r.TransferEncoding)
 }
--- history/request.go
+++ history/request.go
@@ -2,173 +2,75 @@ package history
 
 import (
 	"net/http"
-	"net/url"
-	"sort"
 	"strings"
 
-	"../extractor"
 	"github.com/jinzhu/gorm"
 )
 
 type Request struct {
 	gorm.Model
-	Identifier       string
-	IsEdit           bool
-	Host             string
-	Method           string
-	URL              string
-	Proto            string
-	ContentLength    int64
-	TransferEncoding string
+	Identifier string
+	IsEdit     bool
+	Host       string
+	Method     string
+	URL        string
+	Path       string
+	Proto      string
 }
 
 func (r *Request) SetRequest(req *http.Request, bstr string) {
-	headerKey := []string{}
-	for k := range req.Header {
-		headerKey = append(headerKey, k)
-	}
-	sort.Strings(headerKey)
-	for _, v := range headerKey {
-		heade :=
-			RequestHeader{
-				Identifier: r.Identifier,
-				IsEdit:     r.IsEdit,
-				Method:     req.Method,
-				Host:       req.Host,
-				Path:       req.URL.Path,
-			}
-		heade.SetHeader(v, strings.Join(req.Header[v], ","))
-	}
-	fordata := func(datas []string) {
-		for _, ds := range datas {
-			data :=
-				RequestData{
-					Identifier: r.Identifier,
-					IsEdit:     r.IsEdit,
-					Method:     req.Method,
-					Host:       req.Host,
-					Path:       req.URL.Path,
-				}
-			d := strings.Split(ds, "=")
-			if len(d) > 0 {
-				if len(d) > 1 {
-					data.SetData(d[0], d[1])
-				}
-			}
-		}
-	}
-
-	for _, datas := range [][]string{strings.Split(req.URL.RawQuery, "&"), strings.Split(bstr, "&")} {
-		fordata(datas)
-	}
 	db.Table = Request{}
-	r.Host, r.Method, r.Proto, r.URL, r.ContentLength, r.TransferEncoding =
-		req.Host, req.Method, req.Proto, req.URL.String(), req.ContentLength, strings.Join(req.TransferEncoding, ",")
+	r.Host, r.Method, r.Proto, r.URL, r.Path =
+		req.Host, req.Method, req.Proto, req.URL.String(), req.URL.Path
 	db.Insert(r)
-}
-
-func (r *Request) GetHost() []Request {
-	db.Table = Request{}
-	reqdb := db.OpenDatabase()
-	var request []Request
-	reqdb.
-		Select("DISTINCT host,host").
-		Find(&request)
-	return request
-}
-
-func (r *Request) GetRequest(host string) []http.Request {
-	host = "%" + host + "%"
-	db.Table = Request{}
-	reqdb := db.OpenDatabase()
-	var request []Request
-	reqdb.
-		Select("Distinct method,url,proto").
-		Where("host LIKE ?", host).
-		Find(&request)
-	retReq := []http.Request{}
-	for _, r := range request {
-		u, _ := url.Parse(r.URL)
-		tr := http.Request{
-			Method: r.Method,
-			Host:   r.Host,
-			URL:    u,
-			Proto:  r.Proto,
-			Header: http.Header{},
-		} //tmprequest
-		rh, rd, strs := RequestHeader{}, RequestData{}, []string{}
-		for _, h := range rh.GetHeader(tr.URL.Host, tr.URL.Path, r.Method) {
-			if h.Name == "Cookie" {
-				continue
-			}
-			tr.Header.Add(h.Name, h.Value)
-		}
-
-		for _, d := range rd.GetData(tr.URL.Host, tr.URL.Path, r.Method) {
-			strs = append(strs, d.Name+"="+d.Value)
-		}
-		tr.Body = extractor.GetIOReadCloser(strings.Join(strs, "&"))
-		retReq = append(retReq, tr)
-	}
-	return retReq
 }
 
 type RequestHeader struct {
 	gorm.Model
 	Identifier string
-	Method     string
-	Host       string
-	Path       string
 	Name       string
 	Value      string
 	IsEdit     bool
 }
 
-func (r *RequestHeader) SetHeader(name string, value string) {
-	r.Name = name
-	r.Value = value
+func (r *Request) SetHeader(header http.Header) {
+	ise := r.IsEdit
+	id := r.Identifier
 	db.Table = RequestHeader{}
-	db.Insert(r)
-}
-
-func (r *RequestHeader) GetHeader(host string, path string, method string) []RequestHeader {
-	db.Table = RequestHeader{}
-	reqdb := db.OpenDatabase()
-	var requestHeader []RequestHeader
-	reqdb.
-		Select("*").
-		Where("host = ? AND path = ? AND method = ?", host, path, method).
-		Group("name").
-		Find(&requestHeader)
-	return requestHeader
+	for name, value := range header {
+		insHeader := &RequestHeader{
+			Identifier: id,
+			Name:       name,
+			Value:      strings.Join(value, ","),
+			IsEdit:     ise,
+		}
+		db.Insert(insHeader)
+	}
 }
 
 type RequestData struct {
 	gorm.Model
-	Identifier string
-	Method     string
-	Host       string
-	Path       string
-	Name       string
-	Value      string
-	IsEdit     bool
+	Identifier       string
+	Name             string
+	Value            string
+	TransferEncoding string
+	IsEdit           bool
 }
 
-func (r *RequestData) SetData(name string, value string) {
-	r.Name = name
-	r.Value = value
+func (r *Request) SetData(bstr string, length int64, enctype []string) {
+	id := r.Identifier
+	ise := r.IsEdit
 	db.Table = RequestData{}
-	db.Insert(r)
-}
+	for _, params := range strings.Split(bstr, "&") {
+		param := strings.Split(params, "=")
+		data := &RequestData{
+			Identifier:       id,
+			Name:             param[0],
+			Value:            strings.Join(param[1:], "="),
+			TransferEncoding: strings.Join(enctype, ","),
+			IsEdit:           ise,
+		}
+		db.Insert(data)
 
-func (r *RequestData) GetData(host string, path string, method string) []RequestData {
-	db.Table = RequestData{}
-	reqdb := db.OpenDatabase()
-	var requestData []RequestData
-	reqdb.
-		Select("*").
-		Where("host = ? AND path = ? AND method = ?", host, path, method).
-		Group("name").
-		Find(&requestData)
-	return requestData
+	}
 }
