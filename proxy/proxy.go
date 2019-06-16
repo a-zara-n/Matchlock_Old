602bf776af21c72737c9961c106fcc180a7dfa05
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-06-16 22:04:35 +0900

    Function separation

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -17,29 +17,20 @@ type proxyInfo struct {
 }
 
 func (p *proxyInfo) Run() {
-	var isHost = []bool{}
 	p.proxy = goproxy.NewProxyHttpServer()
 	p.proxy.Verbose = false
 	AddWhiteList(`^[0-9a-zA-Z]*\.?(localhost)(\.+[0-9a-zA-Z]+)*$`)
-	p.proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
-	reqchan := p.channels.Request
-	reschan := p.channels.Response
-	p.proxy.OnRequest().DoFunc(
-		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			i := whitelistMatch(r.Host)
-			isHost = append(isHost, i)
-			if i {
-				reqchan.ProxToHMgSignal <- r
-				resp := <-reschan.ProxToHMgSignal
-				return nil, resp
-			}
-			return r, nil
-		})
+	p.proxy.OnRequest().DoFunc(p.matchlockLogic)
 	http.ListenAndServe(":10080", p.proxy)
 }
 
-func requestHandler(r *http.Request) (*http.Request, error) {
-	return nil, nil
+func (p *proxyInfo) matchlockLogic(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	if whitelistMatch(r.Host) {
+		p.channels.Request.ProxToHMgSignal <- r
+		resp := <-p.channels.Response.ProxToHMgSignal
+		return nil, resp
+	}
+	return r, nil
 }
 
 func NewProxy(m *channel.Matchlock) Proxy {
@@ -68,7 +59,6 @@ func UpdataWhiteList(domains []string) bool {
 }
 
 func whitelistMatch(host string) bool {
-
 	for _, d := range whitelist {
 		if check_regexp(d, host) {
 			return true
