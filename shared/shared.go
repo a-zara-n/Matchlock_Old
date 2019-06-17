bad22d6cff0b8d30c4af3a9a1d69b7ec77f7202f
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-06-17 13:09:43 +0900

    Separation of shared functions

---
+++ shared/shared.go
@@ -0,0 +1,52 @@
+package shared
+
+import (
+	"io"
+	"regexp"
+	"sort"
+	"strings"
+
+	"github.com/WestEast1st/Matchlock/extractor"
+)
+
+func Merge(m1, m2 map[string]string) map[string]string {
+	ans := map[string]string{}
+	for k, v := range m1 {
+		ans[k] = v
+	}
+	for k, v := range m2 {
+		ans[k] = v
+	}
+	return (ans)
+}
+
+func SeparationOfIOReadCloser(b io.ReadCloser) (string, io.ReadCloser) {
+	bodyOfStr := extractor.GetStringBody(b)
+	b = extractor.GetIOReadCloser(bodyOfStr)
+	return bodyOfStr, b
+}
+
+func CheckRegexp(reg, str string) bool {
+	return regexp.MustCompile(reg).Match([]byte(str))
+}
+
+func GetKeys(maps map[string][]string) []string {
+	ret := []string{}
+	for key := range maps {
+		ret = append(ret, key)
+	}
+	sort.Strings(ret)
+	return ret
+}
+
+func QuoteEscape(str string) string {
+	str = strings.Replace(str, `"`, `\\\"`, -1)
+	str = strings.Replace(str, "'", `\\\'`, -1)
+	return str
+}
+
+func RecursiveExec(slice []string, fun func(slice []string)) {
+	if len(slice) > 1 {
+		go fun(slice[1:])
+	}
+}
