fb04ba497a30ed3648f260c58ea51f5672fa763b
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-07 21:00:13 +0900

    add Basic operation of ResponseBodyRepositry

--- src/infrastructure/persistence/datastore/response_body_repository.go
+++ src/infrastructure/persistence/datastore/response_body_repository.go
@@ -1,1 +1,58 @@
 package datastore
+
+import (
+	"github.com/a-zara-n/Matchlock/src/domain/entity"
+	"github.com/a-zara-n/Matchlock/src/domain/repository"
+	"github.com/jinzhu/gorm"
+)
+
+//ResponseBodyRepositry は保存用のRepositryです
+type ResponseBodyRepositry struct {
+	historyCommon
+}
+
+//ResponseBody は保存用のschemaです
+type ResponseBody struct {
+	gorm.Model
+	Identifier string
+	Body       string
+	Encodetype string
+	Length     int64
+}
+
+//NewResponseBodyRepositry はResponseBodyRepositryを取得する
+func NewResponseBodyRepositry(Identifier string, IsEdit bool, db *gorm.DB) repository.ResponseBodyRepositry {
+	return &ResponseBodyRepositry{
+		historyCommon{Identifier, IsEdit, db},
+	}
+}
+
+//SetIsEdit は編集のフラグを書き換えることができます
+func (r *ResponseBodyRepositry) SetIsEdit(flag bool) { r.IsEdit = flag }
+
+//SetIdentifier はIdentifierを書き換えることができます
+func (r *ResponseBodyRepositry) SetIdentifier(id string) { r.Identifier = id }
+
+//Insert はBodyを保存します
+func (r *ResponseBodyRepositry) Insert(e *entity.Body) bool {
+	insertData := &ResponseBody{
+		Identifier: r.Identifier,
+		Body:       e.Body,
+		Encodetype: e.Encodetype,
+		Length:     e.Length,
+	}
+	r.DB.Create(insertData)
+	return true
+}
+
+//Select はentity.Bodyを取得出来る
+func (r *ResponseBodyRepositry) Select() *entity.Body {
+	rets := []*ResponseBody{}
+	r.DB.Where("Identifier = ?", r.Identifier).Find(rets)
+	retentity := &entity.Body{
+		Body:       rets[0].Body,
+		Encodetype: rets[0].Encodetype,
+		Length:     rets[0].Length,
+	}
+	return retentity
+}
