644776b0a4cedcb18e015be37a63d62576060f67
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-07 23:09:45 +0900

    Add Use case of websocket  

--- src/application/usecase/websocket_connect_usecase.go
+++ src/application/usecase/websocket_connect_usecase.go
@@ -1,8 +1,28 @@
 package usecase
 
-type WebSocketUsecase interface{}
-type websocketusecase struct{}
+import (
+	"net/http"
+
+	"github.com/a-zara-n/Matchlock/src/domain/aggregate"
+)
+
+type WebSocketUsecase interface {
+	GetHTTPRequestByRequest(data string) *http.Request
+	GetHTTPRequestByString(data *http.Request) string
+}
+type websocketusecase struct {
+	request aggregate.Request
+}
 
 func NewWebSocketUsecase() WebSocketUsecase {
-	return &websocketusecase{}
+	return &websocketusecase{request: aggregate.Request{}}
+}
+
+func (wsu *websocketusecase) GetHTTPRequestByRequest(data string) *http.Request {
+	wsu.request.SetHTTPRequestByString(data)
+	return wsu.request.GetHTTPRequestByRequest()
+}
+func (wsu *websocketusecase) GetHTTPRequestByString(data *http.Request) string {
+	wsu.request.SetHTTPRequestByRequest(data)
+	return wsu.request.GetHTTPRequestByString()
 }
