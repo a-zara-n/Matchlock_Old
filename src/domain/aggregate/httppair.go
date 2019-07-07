166ded232a262ba98381f0cc15d51abc691f6dfb
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-07 23:10:29 +0900

    Define JSON settings required for interaction

--- src/domain/aggregate/httppair.go
+++ src/domain/aggregate/httppair.go
@@ -10,3 +10,22 @@ type HTTPPair struct {
 	EditRequest Request
 	Response    Response
 }
+
+//HTTPDataDefinitionByJSON はWSで利用されるJSONのデータ定義
+type HTTPDataDefinitionByJSON struct {
+	Identifier         string `json:"Identifier"`
+	RequestMethod      string `json:"Method"`
+	RequestPath        string `json:"Path"`
+	RequestProto       string `json:"Proto"`
+	RequestHost        string `json:"Host"`
+	RequestHeaders     string `json:"Header"`
+	RequestParam       string `json:"Param"`
+	RequestEditMethod  string `json:"EditMethod"`
+	RequestEditPath    string `json:"EditPath"`
+	RequestEditProto   string `json:"EditProto"`
+	RequestEditHost    string `json:"EditHost"`
+	RequestEditHeaders string `json:"EditHeader"`
+	RequestEditParam   string `json:"EditParam"`
+	ResponseHeaders    string `json:"ResHeader"`
+	Body               string `json:"ReqBody"`
+}
