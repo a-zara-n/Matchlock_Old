273e74c812ebd94390e98ff5cf2f0c69923bec62
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-13 01:29:20 +0900

    Test data separation

--- src/domain/entity/data_test.go
+++ src/domain/entity/data_test.go
@@ -5,31 +5,11 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/a-zara-n/Matchlock/src/testdata"
 )
 
-var (
-	testingDataEntity = &Data{Data: map[string]interface{}{}}
-	testData          = map[string]interface{}{
-		"FORM": map[string]interface{}{
-			"Raw":    `name=hoge&age=20&like=mikan`,
-			"Keys":   []string{"name", "age", "like"},
-			"Result": map[string]interface{}{"name": "hoge", "age": "20", "like": "mikan"},
-			"Fetch":  `age=20&like=mikan&name=hoge`,
-		},
-		"JSON_Success": map[string]interface{}{
-			"Raw":    `{"age":20,"lang":["jp","en","fr"],"like":"mikan","name":"hoge"}`,
-			"Keys":   []string{"name", "age", "like", "lang"},
-			"Result": map[string]interface{}{"name": "hoge", "age": float64(20), "like": "mikan", "lang": []interface{}{"jp", "en", "fr"}},
-			"Fetch":  `{"age":20,"lang":["jp","en","fr"],"like":"mikan","name":"hoge"}`,
-		},
-		"JSON_Fail": map[string]interface{}{
-			"Raw":    `{"name":hoge}`,
-			"Keys":   []string{},
-			"Result": map[string]interface{}{},
-			"Fetch":  `{}`,
-		},
-	}
-)
+var testingDataEntity = &Data{Data: map[string]interface{}{}}
 
 func TestGetKeysOfDataEntity(t *testing.T) {
 	NothingIsIncluded := testingDataEntity.GetKeys()
@@ -56,34 +36,32 @@ func TestAddDataAndRemoveDataOfDataEntity(t *testing.T) {
 }
 
 func TestSetDataAndFetchDataOfDataEntity(t *testing.T) {
-	for casestring, data := range testData {
-		trance := data.(map[string]interface{})
-		testingDataEntity.SetData(trance["Raw"].(string))
-		res := trance["Result"].(map[string]interface{})
+	for casestring, data := range testdata.TestQuery {
+		testingDataEntity.SetData(data["Raw"].(string))
+		res := data["Result"].(map[string]interface{})
 		for n, v := range res {
 			if !reflect.DeepEqual(testingDataEntity.Data[n], v) {
 				t.Errorf("%v :値が異なっています", casestring)
 			}
 		}
-		if testingDataEntity.FetchData() != trance["Fetch"] {
-			t.Errorf("%v :出力が異なります %v != %v", casestring, testingDataEntity.FetchData(), trance["Fetch"])
+		if testingDataEntity.FetchData() != data["Fetch"] {
+			t.Errorf("%v :出力が異なります %v != %v", casestring, testingDataEntity.FetchData(), data["Fetch"])
 		}
 	}
 }
 
 //Test SetDataByHTTPBody And FetchData Of DataEntity
 func TestSetDataByHTTPBodyAndFetchDataOfDataEntity(t *testing.T) {
-	for casestring, data := range testData {
-		trance := data.(map[string]interface{})
-		testingDataEntity.SetDataByHTTPBody(ioutil.NopCloser(strings.NewReader(trance["Raw"].(string))))
-		res := trance["Result"].(map[string]interface{})
+	for casestring, data := range testdata.TestQuery {
+		testingDataEntity.SetDataByHTTPBody(ioutil.NopCloser(strings.NewReader(data["Raw"].(string))))
+		res := data["Result"].(map[string]interface{})
 		for n, v := range res {
 			if !reflect.DeepEqual(testingDataEntity.Data[n], v) {
 				t.Errorf("%v :値が異なっています", casestring)
 			}
 		}
-		if testingDataEntity.FetchData() != trance["Fetch"] {
-			t.Errorf("%v :出力が異なります %v != %v", casestring, testingDataEntity.FetchData(), trance["Fetch"])
+		if testingDataEntity.FetchData() != data["Fetch"] {
+			t.Errorf("%v :出力が異なります %v != %v", casestring, testingDataEntity.FetchData(), data["Fetch"])
 		}
 	}
 }
