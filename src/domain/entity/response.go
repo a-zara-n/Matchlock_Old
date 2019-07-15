cc27c7f67a0f9cfa6de34e313aa8f73e4024b599
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-16 00:47:05 +0900

    Add method "Set"

--- src/domain/entity/response.go
+++ src/domain/entity/response.go
@@ -1,5 +1,7 @@
 package entity
 
+import "net/http"
+
 //ResponseInfo は
 type ResponseInfo struct {
 	Status     string // e.g. "200 OK"
@@ -7,4 +9,12 @@ type ResponseInfo struct {
 	Proto      string // e.g. "HTTP/1.0"
 	ProtoMajor int    // e.g. 1
 	ProtoMinor int    // e.g. 0
+}
+
+func (r *ResponseInfo) Set(resp *http.Response) {
+	r.Status = resp.Status
+	r.StatusCode = resp.StatusCode
+	r.Proto = resp.Proto
+	r.ProtoMajor = resp.ProtoMajor
+	r.ProtoMinor = resp.ProtoMinor
 }
