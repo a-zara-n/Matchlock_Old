c54e3ac6d95eba7b45bfa731914d309a98cb4626
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-18 00:06:13 +0900

    add request aggregate And testcode

--- src/domain/aggregate/request.go
+++ src/domain/aggregate/request.go
@@ -2,6 +2,8 @@ package aggregate
 
 import (
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/a-zara-n/Matchlock/src/domain/entity"
 )
@@ -14,38 +16,60 @@ type Request struct {
 }
 
 //NewHTTPRequestByRequest はhttp.Requestを利用しaggregate.Requestを取得できます
-func NewHTTPRequestByRequest(req *http.Request) *Request {
-	request := &Request{
+func NewHTTPRequestByRequest(request *http.Request) *Request {
+	requestuest := &Request{
 		Info:   &entity.RequestInfo{},
 		Header: &entity.HTTPHeader{},
 		Data:   &entity.Data{},
 	}
-	request.SetHTTPRequestByRequest(req)
-	return request
+	requestuest.SetHTTPRequestByRequest(request)
+	return requestuest
 }
 
 //SetHTTPRequestByRequest はhttp.Requestを元に設定をします
-func (r *Request) SetHTTPRequestByRequest(req *http.Request) {
-	r.Info.SetRequestINFO(req)
-	r.Header.SetHTTPHeader(req.Header)
-	if req.Method == "POST" {
-		req.Body = r.Data.SetDataByHTTPBody(req.Body)
+func (req *Request) SetHTTPRequestByRequest(request *http.Request) {
+	req.Info.SetRequestINFO(request)
+	req.Header.SetHTTPHeader(request.Header)
+	if request.Method == "POST" {
+		request.Body = req.Data.SetDataByHTTPBody(request.Body)
 		return
 	}
-	r.Data.SetData(req.URL.RawQuery)
+	req.Data.SetData(request.URL.RawQuery)
 }
 
 //SetHTTPRequestByString はstringを元に設定をします
-func (r *Request) SetHTTPRequestByString(req string) {
-
+func (req *Request) SetHTTPRequestByString(request string) {
+	slicehttprequest := strings.Split(request, "\n")
+	req.Info.SetStatusLine(slicehttprequest[0])
+	req.Header.SetStringHeader(strings.Join(slicehttprequest[1:len(slicehttprequest)-2], "\n"))
+	req.Data.SetData(slicehttprequest[len(slicehttprequest)-1])
 }
 
 //GetHTTPRequestByRequest はhttp.Requestを生成します
-func (r *Request) GetHTTPRequestByRequest() *http.Request {
-	return &http.Request{}
+func (req *Request) GetHTTPRequestByRequest() *http.Request {
+	proto := strings.Split(strings.Split(req.Info.Proto, "/")[1], ".")
+	major, err := strconv.Atoi(proto[0])
+	if err != nil {
+		major = 1
+	}
+	minor, err := strconv.Atoi(proto[1])
+	if err != nil {
+		minor = 0
+	}
+	return &http.Request{
+		Host:          req.Info.Host,
+		Method:        req.Info.Method,
+		URL:           req.Info.URL,
+		Proto:         req.Info.Proto,
+		ProtoMajor:    major,
+		ProtoMinor:    minor,
+		Header:        req.Header.Header,
+		Body:          req.Data.GetIoReadCloser(),
+		ContentLength: int64(len(req.Data.FetchData())),
+	}
 }
 
 //GetHTTPRequestByString は文字列のhttp requestを生成します
-func (r *Request) GetHTTPRequestByString() string {
-	return ""
+func (req *Request) GetHTTPRequestByString() string {
+	return req.Info.GetStatusLine() + "\n" + req.Header.GetStringHeader() + "\n\n" + req.Data.FetchData()
 }
