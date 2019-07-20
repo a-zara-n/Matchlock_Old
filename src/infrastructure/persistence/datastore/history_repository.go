83da22ce8c152f47871b1c357f30eb7fdfa97416
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-20 14:03:53 +0900

    Move the definition of JSON

--- src/infrastructure/persistence/datastore/history_repository.go
+++ src/infrastructure/persistence/datastore/history_repository.go
@@ -1,7 +1,28 @@
 package datastore
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/a-zara-n/Matchlock/src/domain/repository"
+	"github.com/jinzhu/gorm"
+)
 
 type historyCommon struct {
 	*gorm.DB
+}
+
+//HTTPHistoryDefinitionJSON はAPIで利用されるJSONのschemaを定義しています
+type HTTPHistoryDefinitionJSON struct {
+	ID         int64  `json:"ID"`
+	Identifier string `json:"Identifier"`
+	Method     string `json:"Method"`
+	Host       string `json:"Host"`
+	Path       string `json:"Path"`
+	URL        string `json:"URL"`
+	Param      string `json:"Param"`
+}
+
+//NewHTTPHistory はRequestDataを取得する
+func NewHTTPHistory(db *gorm.DB) repository.RequestDataRepositry {
+	return &RequestDataRepositry{
+		historyCommon{DB: db},
+	}
 }
