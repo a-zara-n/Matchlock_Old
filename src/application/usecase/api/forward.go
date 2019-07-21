aa012b94a57ea6520557dce2c7f094092ac4d37c
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-21 19:07:09 +0900

    Transfer the echo logic to the interface

--- src/application/usecase/api/forward.go
+++ src/application/usecase/api/forward.go
@@ -1,29 +1,25 @@
 package api
 
 import (
-	"net/http"
-
 	"github.com/a-zara-n/Matchlock/src/domain/value"
-
-	"github.com/labstack/echo"
 )
 
 //Forward は止めるかとどうか考える
 type Forward interface {
-	ChangeForward(c echo.Context) (bool, error)
+	ChangeForward() (bool, error)
 }
 
 type forward struct {
 	flag *value.Forward
 }
 
-func (f *forward) ChangeForward(c echo.Context) (bool, error) {
+func (f *forward) ChangeForward() (bool, error) {
 	if f.flag.Get() {
 		f.flag.Set(false)
 	} else {
 		f.flag.Set(true)
 	}
-	return f.flag.Get(), c.JSON(http.StatusOK, "{status: \"OK\"}")
+	return f.flag.Get(), nil
 }
 
 //NewForward は
