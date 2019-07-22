eed53068fddead4c5e6bf269435b2732d4512c8a
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-22 22:38:17 +0900

    Add whitelist entity

--- src/application/usecase/api_usecase.go
+++ src/application/usecase/api_usecase.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"github.com/a-zara-n/Matchlock/src/application/usecase/api"
+	"github.com/a-zara-n/Matchlock/src/domain/entity"
 	"github.com/a-zara-n/Matchlock/src/domain/repository"
 	"github.com/a-zara-n/Matchlock/src/domain/value"
 )
@@ -11,10 +12,11 @@ type APIUsecase struct {
 	api.ForwordInterface
 	api.HistoryInterface
 	api.MessageInterface
+	api.WhiteListInterface
 }
 
-func NewAPIUsecase(f *value.Forward, memreq repository.RequestRepositry, memres repository.ResponseRepositry, history repository.HistoryRepository, message repository.HTTPMessageRepository) *APIUsecase {
+func NewAPIUsecase(f *value.Forward, whitelist *entity.WhiteList, history repository.HistoryRepository, message repository.HTTPMessageRepository) *APIUsecase {
 	return &APIUsecase{
-		api.NewForword(f), api.NewHistory(history), api.NewMessage(message),
+		api.NewForword(f), api.NewHistory(history), api.NewMessage(message), api.NewWhiteList(whitelist),
 	}
 }
