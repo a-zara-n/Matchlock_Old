76496fc0f2c9fcac06386d8f70b4825b86a2e9f3
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-22 13:34:20 +0900

    Define get message

--- src/domain/repository/http_message_repository.go
+++ src/domain/repository/http_message_repository.go
@@ -1,9 +1,22 @@
 package repository
 
-import (
-	"github.com/a-zara-n/Matchlock/src/domain/aggregate"
-)
-
 type HTTPMessageRepository interface {
-	Fetch(Identifier string, IsEdit bool) *aggregate.HTTPMessages
+	Fetch(Identifier string) []HTTPMessageDefinitionJSON
+}
+type HTTPMessageDefinitionJSON struct {
+	Identifier         string `json:"Identifier"`
+	RequestMethod      string `json:"Method"`
+	RequestPath        string `json:"Path"`
+	RequestProto       string `json:"Proto"`
+	RequestHost        string `json:"Host"`
+	RequestHeaders     string `json:"Header"`
+	RequestParam       string `json:"Param"`
+	RequestEditMethod  string `json:"EditMethod"`
+	RequestEditPath    string `json:"EditPath"`
+	RequestEditProto   string `json:"EditProto"`
+	RequestEditHost    string `json:"EditHost"`
+	RequestEditHeaders string `json:"EditHeader"`
+	RequestEditParam   string `json:"EditParam"`
+	ResponseHeaders    string `json:"ResHeader"`
+	Body               string `json:"ReqBody"`
 }
