620cfe7562bfb58ca7a30605f640e1ba0cf7d78b
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-07-22 13:37:16 +0900

    Add API routing

--- src/interfaces/httpserver/apihandler/api.go
+++ src/interfaces/httpserver/apihandler/api.go
@@ -1,14 +1,26 @@
 package apihandler
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/a-zara-n/Matchlock/src/application/usecase"
 	"github.com/labstack/echo"
 )
 
+//message json struct
+type Message struct {
+	Type string `json:"Type"`
+	Data string `json:"Data"`
+}
+
+type APIresponse struct {
+	Data interface{} `json:"Data"`
+}
 type API interface {
 	ChangeForward(c echo.Context) error
+	FetchHistory(c echo.Context) error
+	FetchMessage(c echo.Context) error
 }
 type api struct {
 	usecase *usecase.APIUsecase
@@ -21,5 +33,36 @@ func NewAPIHandler(a *usecase.APIUsecase) API {
 func (api *api) ChangeForward(c echo.Context) error {
 	retbool, _ := api.usecase.ChangeForward()
 	c.JSON(http.StatusOK, map[string]interface{}{"status": retbool})
+	return nil
+}
+
+func (api *api) FetchHistory(c echo.Context) error {
+	var (
+		historys interface{}
+		response = c.Response()
+	)
+	switch c.Param("type") {
+	case "all":
+		historys = api.usecase.FetchAll()
+	}
+	responsedata, err := json.Marshal(APIresponse{Data: historys})
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+	response.Header().Set("Content-Type", "application/json")
+	response.Write(responsedata)
+	return nil
+}
+
+func (api *api) FetchMessage(c echo.Context) error {
+	var response = c.Response()
+	responsedata, err := json.Marshal(APIresponse{Data: api.usecase.FetchMessage(c.Param("identifier"))})
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+	response.Header().Set("Content-Type", "application/json")
+	response.Write(responsedata)
 	return nil
 }
--- src/interfaces/httpserver/router.go
+++ src/interfaces/httpserver/router.go
@@ -9,5 +9,7 @@ func (h *httpServer) router(e *echo.Echo) *echo.Echo {
 	e.GET("/", h.Index)
 	e.GET("/connect", h.WebSocketHandler.ServeHTTP)
 	e.GET("/api/is/forward", h.API.ChangeForward)
+	e.GET("/api/history/:type", h.API.FetchHistory)
+	e.GET("/api/message/:identifier", h.API.FetchMessage)
 	return e
 }
